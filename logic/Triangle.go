ec44f2a1a3bf23a3a95fed67322e59b5911cecf9
panxinyang <1670016749@qq.com>
2022-09-08 22:42:21 +0800

    print_amount

--- logic/Triangle.go
+++ logic/Triangle.go
@@ -232,8 +232,8 @@ func SendOsmoTriTx(ctx *tool.MyContext) {
 			amountin := Min(v.Depth, balAmount-osmo.GAS_FEE)
 			tokenMinOUt := strconv.FormatUint(amountin, 10)
 			fmt.Println("send msg")
-			fmt.Printf("hope profit is: %v:ratio is %v\n", float64(amountin)*v.Ratio, amountin, v.Ratio)
-			ctx.Logger.Infof("hope profit is: %v:ratio is %v\n", float64(amountin)*v.Ratio, amountin, v.Ratio)
+			fmt.Printf("hope profit is: %v:amount is %d:ratio is %v\n", float64(amountin)*v.Ratio, amountin, v.Ratio)
+			ctx.Logger.Infof("hope profit is: %v:amount is %d:ratio is %v\n", float64(amountin)*v.Ratio, amountin, v.Ratio)
 			if float64(amountin)*v.Ratio > float64(osmo.GAS_FEE*3) {
 				resp, err := osmo.SendOsmoTx(ctx, MNEMONIC, OSMO_DENOM, tokenMinOUt, amountin, seq, accnum, v.PoolIds, v.TokenOutDenom)
 				if err != nil {
