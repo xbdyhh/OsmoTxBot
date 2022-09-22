278a7b4d95b1cd7d4d55c2978de6db5522280d95
panxinyang <1670016749@qq.com>
2022-09-23 01:01:43 +0800

    delete_ids_more_5

--- logic/module/pool.go
+++ logic/module/pool.go
@@ -28,7 +28,7 @@ func (p *Path) GetRatio() {
 		(1 - p.Fees)
 }
 func (p Path) GetDepth() uint64 {
-	return uint64(0.001 * float64(p.AmountFrom))
+	return uint64(0.0005 * float64(p.AmountFrom))
 }
 
 type Router struct {
