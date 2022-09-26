f364a9a4f380671c3bc17b7cf9df63e0ac380dcd
panxinyang <1670016749@qq.com>
2022-09-27 01:28:14 +0800

    change_gas_limit

--- tool/osmo/osmosis_tx.go
+++ tool/osmo/osmosis_tx.go
@@ -29,7 +29,7 @@ import (
 )
 
 const (
-	GAS_LIMIT           = 1000000
+	GAS_LIMIT           = 360000
 	GRPC_SERVER_ADDRESS = "65.108.141.109:9090"
 	REST_ADDRESS        = "http://65.108.141.109:1317/"
 	CHAIN_ID            = "osmosis-1"
