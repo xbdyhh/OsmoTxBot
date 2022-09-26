92bac6361baa6204efc6477c8e61376d92eea5f3
panxinyang <1670016749@qq.com>
2022-09-27 00:19:18 +0800

    change_gas

--- logic/Triangle.go
+++ logic/Triangle.go
@@ -116,9 +116,6 @@ func (p PoolMap) FindPath(ctx *tool.MyContext, oldids []uint64, depth uint64, ra
 		for _, path := range patharr {
 			depth2 := uint64(float64(path.GetDepth()) / ratio)
 			newrouters := make([]module.Router, 0, 0)
-			if len(ids) > 3 && ids[0] == 15 && ids[1] == 13 && ids[2] == 498 && ids[3] == 807 {
-				fmt.Println(p[denom])
-			}
 			if !IsIdIn(ids, path.ID) {
 				newrouters = p.FindPath(ctx, append(ids, path.ID), MinDepth(depth2, depth), ratio*path.Ratio, append(denoms, key), key)
 			}
@@ -277,15 +274,15 @@ func SendOsmoTriTx(ctx *tool.MyContext) {
 	for i, v := range TransactionRouters {
 		amountin := Min(v.Depth, balAmount)
 		if amountin == balAmount {
-			amountin -= osmo.GAS_FEE * uint64(len(v.PoolIds))
+			amountin -= osmo.GAS_FEE + uint64(len(v.PoolIds))
 		}
-		tokenMinOut := strconv.FormatUint(amountin+uint64(len(v.PoolIds))*osmo.GAS_FEE, 10)
+		tokenMinOut := strconv.FormatUint(amountin+uint64(len(v.PoolIds))+osmo.GAS_FEE, 10)
 		//判断利润是否达标
-		if float64(amountin)*(v.Ratio-1) > float64(osmo.GAS_FEE*int64(len(v.PoolIds))) {
+		if float64(amountin)*(v.Ratio-1) > float64(osmo.GAS_FEE+int64(len(v.PoolIds))) {
 			fmt.Printf("hope profit is: %v:amount is %d:ratio is %v:bal is %v:depth is %v \n",
 				float64(amountin)*(v.Ratio-1), amountin, v.Ratio, balAmount, v.Depth)
 			ctx.Logger.Debugf("hope profit is: %v:amount is %d:ratio is %v\n", float64(amountin)*(v.Ratio-1), amountin-osmo.GAS_FEE, v.Ratio)
-			resp, err := osmo.SendOsmoTx(ctx, MNEMONIC, OSMO_DENOM, tokenMinOut, amountin, seq, accnum, v.PoolIds, v.TokenOutDenom, int64(len(v.PoolIds))*osmo.GAS_FEE)
+			resp, err := osmo.SendOsmoTx(ctx, MNEMONIC, OSMO_DENOM, tokenMinOut, amountin, seq, accnum, v.PoolIds, v.TokenOutDenom, int64(len(v.PoolIds))+osmo.GAS_FEE)
 			if err != nil {
 				ctx.Logger.Errorf("%d tx err:%v", i, err)
 				continue
@@ -294,7 +291,7 @@ func SendOsmoTriTx(ctx *tool.MyContext) {
 				continue
 			} else if resp.Code == 0 {
 				seq++
-				balAmount -= amountin + osmo.GAS_FEE*uint64(len(v.PoolIds))
+				balAmount -= amountin + osmo.GAS_FEE + uint64(len(v.PoolIds))
 				txs = append(txs, resp.TxHash)
 
 			} else if resp.Code == 32 {
@@ -307,7 +304,7 @@ func SendOsmoTriTx(ctx *tool.MyContext) {
 			}
 
 		}
-		if balAmount <= osmo.GAS_FEE*3 {
+		if balAmount <= osmo.GAS_FEE*2 {
 			break
 		}
 	}
