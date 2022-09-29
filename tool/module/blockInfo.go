99c6b4365ad48b381c55f3b7876a35090df99886
panxinyang <1670016749@qq.com>
2022-09-29 13:32:00 +0800

    push_all

--- tool/module/blockInfo.go
+++ tool/module/blockInfo.go
@@ -1,21 +1,64 @@
 package module
 
+import "time"
+
 type BlockInfo struct {
-	Height string `json:"height"`
-	Result Result `json:"result"`
+	BlockID BlockID `json:"block_id"`
+	Block   Block   `json:"block"`
 }
-type PubKey struct {
-	Type  string `json:"type"`
-	Value string `json:"value"`
+type Parts struct {
+	Total int    `json:"total"`
+	Hash  string `json:"hash"`
 }
-type Validators struct {
-	Address          string `json:"address"`
-	PubKey           PubKey `json:"pub_key"`
-	ProposerPriority string `json:"proposer_priority"`
-	VotingPower      string `json:"voting_power"`
+type BlockID struct {
+	Hash  string `json:"hash"`
+	Parts Parts  `json:"parts"`
 }
-type Result struct {
-	BlockHeight string       `json:"block_height"`
-	Validators  []Validators `json:"validators"`
-	Total       string       `json:"total"`
+type Version struct {
+	Block string `json:"block"`
+	App   string `json:"app"`
+}
+type LastBlockID struct {
+	Hash  string `json:"hash"`
+	Parts Parts  `json:"parts"`
+}
+type Header struct {
+	Version            Version     `json:"version"`
+	ChainID            string      `json:"chain_id"`
+	Height             string      `json:"height"`
+	Time               time.Time   `json:"time"`
+	LastBlockID        LastBlockID `json:"last_block_id"`
+	LastCommitHash     string      `json:"last_commit_hash"`
+	DataHash           string      `json:"data_hash"`
+	ValidatorsHash     string      `json:"validators_hash"`
+	NextValidatorsHash string      `json:"next_validators_hash"`
+	ConsensusHash      string      `json:"consensus_hash"`
+	AppHash            string      `json:"app_hash"`
+	LastResultsHash    string      `json:"last_results_hash"`
+	EvidenceHash       string      `json:"evidence_hash"`
+	ProposerAddress    string      `json:"proposer_address"`
+}
+type Data struct {
+	Txs []string `json:"txs"`
+}
+type Evidence struct {
+	Evidence []interface{} `json:"evidence"`
+}
+type Signatures struct {
+	BlockIDFlag      int       `json:"block_id_flag"`
+	ValidatorAddress string    `json:"validator_address"`
+	Timestamp        time.Time `json:"timestamp"`
+	Signature        string    `json:"signature"`
+}
+type LastCommit struct {
+	Height     string       `json:"height"`
+	Round      int          `json:"round"`
+	BlockID    BlockID      `json:"block_id"`
+	Signatures []Signatures `json:"signatures"`
+}
+type Block struct {
+	Header     Header     `json:"header"`
+	Data       Data       `json:"data"`
+	Evidence   Evidence   `json:"evidence"`
+	LastCommit LastCommit `json:"last_commit"`
 }
